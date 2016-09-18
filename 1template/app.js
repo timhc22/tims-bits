34ca46275baaa29c22ba7606f02fc45f6b13724e
Timothy Coleman <timhc22@gmail.com>
2016-09-18 22:44:55 +0100

    Add comments

--- 1template/app.js
+++ 1template/app.js
@@ -12,14 +12,17 @@ app.set('layout', 'layout');
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(router);
+
 app.listen(app.get('port'), function () {
     console.log('App listening on port:' + app.get('port'));
 });
 
+// routes
 app.get('/', function (req, res) {
     res.render('home', { title: 'Home' });
 });
 
+// api
 router.get('/api/data', function (req, res, next) {
     res.json({data: 'data'});
 });
