5fde6a58a0f429dce172de263f47a50df9af67dd
Timothy Coleman <timhc22@gmail.com>
2016-10-07 22:20:23 +0100

    Add example database insertions

--- database/app.js
+++ database/app.js
@@ -38,6 +38,24 @@ router.get('/api/data', function (req, res, next) {
 });
 
 
+
+
+
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('meow');
+    }
+});
+
+
+
+
+
 var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
